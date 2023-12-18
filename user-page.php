086a4cfde1bc36a3e23fbc74cd8eb81a8a5f731b
Abenezer Teshome <abenezerteshome7d@gmail.com>
2023-12-19 02:22:58 +0300

    First commit

---
+++ user-page.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Page</title>
+    <link rel='stylesheet' href='Css/styles.css'>
+</head>
+<body>
+    <div class="container">
+        <nav class='nav-bar'>
+            <a href="register-form.php" class='btn'>Register</a>
+            <a href="logout.php" class='btn'>Log out</a>
+        </nav>
+        <div class="content">
+            <h3> Hi,   <span>Student</span></h3>
+            <h1> Welcome   <span></span></h1>
+            <p>This is your Student page</p>
+        </div>
+    </div>
+    
+</body>
+</html>
